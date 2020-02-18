4c6ae3d90f33435c74ff2fd781195fe5aa6ffdfa
aK <l@noreply.com>
2020-02-17 23:28:28 -0800

    Update activity_details & DetailsActivity to display movie title and poster
    Note: UI for DetailsActivity is on progress

--- app/src/main/java/com/udacity/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/udacity/popularmovies/DetailsActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 import com.udacity.popularmovies.model.Movie;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -15,16 +18,23 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         TextView movieTitle = findViewById(R.id.movie_title);
+        ImageView moviePoster = findViewById(R.id.image_movie_poster);
+
 
         Intent intent = getIntent();
 
         if(intent.hasExtra("movieOriginalTitle")){
 
             movieTitle.setText(intent.getStringExtra("movieOriginalTitle"));
-            movieTitle.setText(intent.getStringExtra("moviePoster"));
-            movieTitle.setText(intent.getStringExtra("moviePlotSynopsis"));
+
+            Picasso.with(this)
+                    .load(intent.getStringExtra("moviePoster"))
+                    .into(moviePoster);
+
+
+/*            movieTitle.setText(intent.getStringExtra("moviePlotSynopsis"));
             movieTitle.setText(intent.getStringExtra("movieUserRating"));
-            movieTitle.setText(intent.getStringExtra("movieReleaseDate"));
+            movieTitle.setText(intent.getStringExtra("movieReleaseDate"));*/
 
 
 
