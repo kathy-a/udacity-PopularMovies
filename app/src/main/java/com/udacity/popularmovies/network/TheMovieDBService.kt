5797631eaf34af45a17a37c8f1078b33a4f2f025
Kathy A <kathy.adeva@mattel.com>
2020-08-23 21:23:19 -0700

    refactor: Convert TheMovieDBService to kotlin

---
+++ app/src/main/java/com/udacity/popularmovies/network/TheMovieDBService.kt
@@ -0,0 +1,27 @@
+package com.udacity.popularmovies.network
+
+import com.udacity.popularmovies.model.MovieReview
+import com.udacity.popularmovies.model.MovieTrailer
+import com.udacity.popularmovies.model.Movies
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+// Interface for Retrofit Implementation
+interface TheMovieDBService {
+    @GET("/3/discover/movie")
+    fun getData(
+            @Query("api_key") apiKey: String?,
+            @Query("sort_by") sortOrder: String?): Call<Movies?>?
+
+    @GET("/3/movie/{id}/videos")
+    fun getTrailer(
+            @Path("id") id: Int,
+            @Query("api_key") apiKey: String?): Call<MovieTrailer?>?
+
+    @GET("/3/movie/{id}/reviews")
+    fun getReview(
+            @Path("id") id: Int,
+            @Query("api_key") apiKey: String?): Call<MovieReview?>?
+}
